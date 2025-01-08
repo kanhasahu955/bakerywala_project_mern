888879bd766a3ce5e556994bc57dd4920aa80192
kanhasahu955 <ashoksahu8018183830@gmail.com>
2025-01-08 23:09:37 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactElement, FC } from "react";
+
+const App: FC<ReactElement> = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
